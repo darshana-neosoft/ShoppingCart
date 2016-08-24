3f09089cc050e6d6b86380a78b88447555af87b3
darshana-neosoft <darshana.alone@wwindia.com>
2016-08-24 15:05:34 +0530

    Add

--- ShoppingCart/ShoppingCart/urls.py
+++ ShoppingCart/ShoppingCart/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', 'shoppingapp.views.home',name='home'),
+    #url(r'^', 'shoppingapp.views.home',name='home'),
 ]
